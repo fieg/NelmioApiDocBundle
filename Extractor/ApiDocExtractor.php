53a2530947eeacae122c36969af5a712975f469c
Pierre-Yves LEBECQ <py.lebecq@gmail.com>
2013-03-28 23:10:49 +0100

    [Parser] Added support of VersionExclusionStrategy in JMSMetadataParser.

--- Extractor/ApiDocExtractor.php
+++ Extractor/ApiDocExtractor.php
@@ -359,6 +359,7 @@ class ApiDocExtractor
 	$defaults = array(
 	    'class' => '',
 	    'groups' => array(),
+	    'version' => null,
 	);
 
 	// normalize strings
@@ -369,6 +370,11 @@ class ApiDocExtractor
 	// normalize groups
 	if (isset($input['groups']) && is_string($input['groups'])) {
 	    $input['groups'] = array_map('trim', explode(',', $input['groups']));
+	}
+
+	// normalize version
+	if (!array_key_exists('version', $input)) {
+	    $input['version'] = null;
 	}
 
 	return $input += $defaults;
